bfc3fcc687a11997e672d6846bd1fb237988c0fc
sjakos <sjakos@users.noreply.github.com>
2017-02-11 17:21:21 -0600

    Add form for user log-in
    Added in form referencing Bootstrap's style guide to format the inputs

--- login/login.php
+++ login/login.php
@@ -4,7 +4,7 @@
     <title>Sam's Log-in</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CDN for Bootstrap doesn't work with CSS class completion extension using local files
+    <!-- CDN for Bootstrap doesn't work with CSS class completion extension, using local files
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -15,9 +15,23 @@
 <body>
     <div class="container">  
         <div class="row">  
-            <div>
-Placeholder text
-            </div>
+            <form>
+                <div class="form-group">
+                    <label for="userName">User Name</label>
+                    <input id="userName" type="text" class="form-control" placeholder="User Name">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input id="password" type="text" class="form-control" placeholder="Password">
+                </div>
+                <div class="form-check">
+                    <label class="form-check-label">
+                        <input id="rememberCreds" type="checkbox" class="form-check-input">
+                        Remember my credentials
+                    </label>
+                </div>
+                <input id="submit" type="submit" class="btn btn-outline-primary">
+            </form>
         </div>
     </div>
 </body>
