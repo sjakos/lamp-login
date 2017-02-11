b76d7bc8ef70a15a408934a833a32e275fc5175b
sjakos <sjakos@users.noreply.github.com>
2017-02-11 16:34:26 -0600

    Update local CSS to include Bootstrap 4.0.0alpha.6
    Updated the local CSS files to include Bootstrap 4.0.0alpha.6 and switched the active CSS stylesheet for the login page to read from local for auto complete extension

--- login/login.php
+++ login/login.php
@@ -4,9 +4,12 @@
     <title>Sam's Log-in</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CDN for Bootstrap doesn't work with CSS class completion extension using local files
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/customstyle.css">
 </head>
 <body>
