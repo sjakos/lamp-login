76369dda94aef68bca97868351eebd43cd1e927a
sjakos <sjakos@users.noreply.github.com>
2017-02-11 16:02:33 -0600

    Add head tags and Bootstrap CSS
    Added in initial head tags and set Bootstrap as the CSS framework for the page

--- login/login.php
+++ login/login.php
@@ -1,1 +1,14 @@
 <!--Login page project using HTML and PHP-->
+<!DOCTYPE html5>
+<head>
+    <title>Sam's Log-in</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/customstyle.css">
+</head>
+<body>
+    
+</body>
