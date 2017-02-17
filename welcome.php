39f0580ac3c875356df400a31c75e01909191708
sjakos <sjakos@users.noreply.github.com>
2017-02-16 20:59:53 -0600

    Add tags in the head for welcome page

--- welcome.php
+++ welcome.php
@@ -1,2 +1,20 @@
-<?php
-// Welcome page after successful login
+
+<!DOCTYPE html5>
+<html lang="en">
+
+<head>
+    <title>Sam's Welcome page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="css/customstyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
