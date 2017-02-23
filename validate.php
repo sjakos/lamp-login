2aa07f0402bb8f0a6bbb6ab4424d002395db43cb
sjakos <sjakos@users.noreply.github.com>
2017-02-23 12:38:24 -0600

    Spaced code out a bit

--- validate.php
+++ validate.php
@@ -2,6 +2,7 @@
 // Capture login and password and validate
 $nameLength = strlen($_POST["loginUsrNm"]);
 $passLength = strlen($_POST["loginPwd"]);
+
 $invalidMsg = "<h1>User Name or Password is invalid:</h1>";
 $validCheck = 0;
 $nameLimit = [5,100];
@@ -12,11 +13,13 @@ if (($nameLimit[0]<=$nameLength) && ($nameLength<=$nameLimit[1])) {
 } else {
     $invalidMsg .= "<p>Username should be between 5 and 100 characters</p>";
 }
+
 if (($passLimit[0]<=$passLength) && ($passLength<=$nameLimit[1])) {
     $validCheck++;
 } else {
     $invalidMsg .= "<p>User password should be between 5 and 100 characters</p>";
 }
+
 if ($validCheck==2) {
     echo "<h1>All data is valid!</h1>";
 } else {
