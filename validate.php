b0f3a6bfb76555cee79480c73e4e0e791e934abe
sjakos <sjakos@users.noreply.github.com>
2017-02-22 21:16:19 -0600

    Add string length validation for login form data

--- validate.php
+++ validate.php
@@ -1,3 +1,26 @@
 <?php
-// Capture login and password 
-echo sprintf ("User Name is %s<br>Password is %s", $_POST["loginUsrNm"] , $_POST["loginPwd"]) ; 
+// Capture login and password and validate
+$nameLength = strlen($_POST["loginUsrNm"]);
+$passLength = strlen($_POST["loginPwd"]);
+$invalidMsg = "<h1>User Name or Password is invalid:</h1>";
+$validCheck = 0;
+$nameMin = 5;
+$nameMax = 100;
+$passMin = 5;
+$passMax = 100;
+
+if (($nameMin<=$nameLength) && ($nameLength<=$nameMax)) {
+    $validCheck++;
+} else {
+    $invalidMsg .= "<p>Username should be between 5 and 100 characters</p>";
+}
+if (($passMin<=$passLength) && ($passLength<=$passMax)) {
+    $validCheck++;
+} else {
+    $invalidMsg .= "<p>User password should be between 5 and 100 characters</p>";
+}
+if ($validCheck==2) {
+    echo "<h1>All data is valid!</h1>";
+} else {
+    echo "$invalidMsg";
+}
