1c5ad2d82a49525d681ba2f1550a4b4202bc4d00
sjakos <sjakos@users.noreply.github.com>
2017-02-23 20:38:56 -0600

    Refactor Validate to use functions from Helper
    Refactored Validate to use functions created in Helper to make the code more DRY and efficient.

--- validate.php
+++ validate.php
@@ -1,26 +1,24 @@
 <?php
+
+// TODO: read about include, include_once, require and reqiure_once
+include_once './helpers.php';
+
 // Capture login and password and validate
-$nameLength = strlen($_POST["loginUsrNm"]);
-$passLength = strlen($_POST["loginPwd"]);
+$incoming = array(
+    'username' => $_POST['loginUsrNm'],
+    'password' => $_POST['loginPwd'],
+);
 
 $invalidMsg = "<h1>User Name or Password is invalid:</h1>";
 $validCheck = 0;
 $nameLimit = [5,100];
-$passLimit = [5,100];
 
-if (($nameLimit[0]<=$nameLength) && ($nameLength<=$nameLimit[1])) {
-    $validCheck++;
-} else {
-    $invalidMsg .= "<p>Username should be between 5 and 100 characters</p>";
-}
+$checkStrings = array(
+    'username' => ['Your username', $nameLimit], 
+    'password' => ['Your password', $passLimit],
+);
 
-if (($passLimit[0]<=$passLength) && ($passLength<=$nameLimit[1])) {
-    $validCheck++;
-} else {
-    $invalidMsg .= "<p>User password should be between 5 and 100 characters</p>";
-}
-
-if ($validCheck==2) {
+if (ValidateFormStringData($checkStrings, $incoming, $nameLimit, $invalidMsg)) {
     echo "<h1>All data is valid!</h1>";
 } else {
     echo "$invalidMsg";
