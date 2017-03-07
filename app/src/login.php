f60270dfbdfb054995d003f599e5068127bde882
sjakos <sjakos@users.noreply.github.com>
2017-03-07 13:07:04 -0600

    Refactor and split between login file and index

--- app/src/login.php
+++ app/src/login.php
@@ -1,30 +1,12 @@
-<!--Login page project using HTML and PHP-->
-<!DOCTYPE html5>
-<head>
-    <title>Sam's Log-in</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CDN for Bootstrap doesn't work with CSS class completion extension, using local files
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-    <link rel="stylesheet" href="css/customstyle.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-    <div class="container"> 
-            <form class="center-element shadow" id="loginForm" action="validate.php" method="POST">
-                <div class="form-group">
-                    <label for="loginUsrNm">User Name</label>
-                    <input id="loginUsrNm" name="loginUsrNm" type="text" class="form-control" placeholder="User Name">
-                </div>
-                <div class="form-group">
-                    <label for="loginPwd">Password</label>
-                    <input id="loginPwd" name="loginPwd" type="password" class="form-control" placeholder="Password">
-                </div>
-                <button id="submit" type="submit" class="btn btn-block btn-primary">Log in</button>
-            </form>
+<form class="center-element shadow" id="loginForm" action='validate.php' method="POST">
+    <div class="form-group">
+      <label for="loginUsrNm">User Name</label>
+      <input id="loginUsrNm" name="loginUsrNm" type="text" class="form-control" placeholder="User Name">
     </div>
-</body>
+    
+    <div class="form-group">
+      <label for="loginPwd">Password</label>
+      <input id="loginPwd" name="loginPwd" type="password" class="form-control" placeholder="Password">
+    </div>
+      <button id="submit" type="submit" class="btn btn-block btn-primary">Log in</button>
+</form>
--- public/index.php
+++ public/index.php
@@ -1,1 +1,22 @@
 <!--Index page for log-in project using HTML and PHP-->
+<!DOCTYPE html5>
+<head>
+    <title>Sam's Log-in</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CDN for Bootstrap doesn't work with CSS class completion extension, using local files
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="css/customstyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container"> 
+        <?php 
+          include('../app/src/login.php');
+        ?>
+    </div>
+</body>
