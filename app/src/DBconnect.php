86b37b0d3ac8da67812c3c1e4dca3cb3b7685dcd
sjakos <sjakos@users.noreply.github.com>
2017-03-22 21:13:10 -0500

    Create DB connection class and ini file

--- app/src/DBconnect.php
+++ app/src/DBconnect.php
@@ -4,30 +4,17 @@
  * DBconnect class
  */
 
-class DBconnect
+class DBconnect extends PDO
 {   
-    protected $db_host = 'localhost';
-    protected $db_name = 'login_dev';
-    protected $db_user = 'root';
-    //TODO store the password in a secure format
-    protected $db_pass = 'password';
-
-
-    public function __construct( PDO $db = null)
+    public function __construct()
     {
-        $this->db = $db;
-        if ($this->db === null)
-        {
-            $dsn = "mysql:host-{$this->db_host};dbname={$this->db_name}";
-            $this->db = new PDO(
-                $dsn,
-                $this->db_user,
-                $this->db_pass
-            );
-            $this->db->setAttribute(
-                PDO::ATTR_ERRMODE,
-                PDO::ERRMODE_EXCEPTION
-            );
-        }
+        $config = parse_ini_file( __DIR__ . './config.ini');
+        $user = $config['dbuser'];
+        $pass = $config['dbpass'];
+        $dsn = $config['driver'].':host='.$config['dbhost'].';dbname='.$config['dbname'].';';
+        
+            
+        parent::__construct($dsn, $user ,$pass);
     }
 }
+
